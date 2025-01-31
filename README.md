1aaa8701dee8f768ced22300ce22f1dc6177f332
Flames <isaacomenuche@gmail.com>
2025-01-31 02:13:35 +0100

    feat: hng E stage 0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# hng_stage_0
+# HNG Stage 0 API
+
+A simple API that returns basic information for HNG internship.
+
+## API Documentation
+
+**Endpoint**: `GET /`
+
+**Response**:
+
+```json
+{
+  "email": "your-email@example.com",
+  "current_datetime": "2025-01-30T09:30:00Z",
+  "github_url": "https://github.com/yourusername/hng-stage0-api"
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+	const responseData = {
+		email: "isaacomenuche@gmail.com",
+		current_datetime: new Date().toISOString(),
+		github_url: "https://github.com/iflames1/hng_stage_0",
+	};
+
+	res.status(200).json(responseData);
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+})
